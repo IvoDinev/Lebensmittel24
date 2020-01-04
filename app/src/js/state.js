abd21c4cd9eebec38fd9be0b5abda7b61a17009d
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2020-01-04 20:26:37 +0200

    add product to shopping cart

--- app/src/js/state.js
+++ app/src/js/state.js
@@ -12,6 +12,14 @@ let fruitsAndVegs = [
   }
 ];
 
+let milkproducts = [
+  {
+    name: 'Weihenstephan Haltbare Milch  %3,5 Fett 1 L',
+    img: './milch.png',
+    price: 1.3
+  }
+];
+
 if (!state) {
   sessionStorage.setItem('state', JSON.stringify({ selectedItems: [] }));
 }
@@ -24,6 +32,24 @@ let selectItems = name => {
   state.selectedItems.push({ name, value });
   sessionStorage.setItem('state', JSON.stringify(state));
 };
+
+let displayMilkProducts = () => {
+  let container = document.getElementById('milkProducts');
+  milkproducts.forEach(item => {
+    container.innerHTML += `<div class="items">
+    <div class="mprodukt">         
+      <img src="${item.img}">           
+      <p>${item.name}</p>
+      <label>Anzahl        
+        <input id="input_${item.name}" type="number" min="1" step="1" max="100">
+      </label>
+      <button  onclick="selectItems('${item.name}')" class="w3-button w3-xlarge w3-black" data-toggle="tooltip"
+      title="Zum Warenkorb hinzufügen!"> <img src="./addToShoppingCart.png"> </button>
+    </div>
+  </div>`;
+  });
+};
+
 let displayFruits = () => {
   let container = document.getElementById('fruitsAndVegs');
   fruitsAndVegs.forEach(item => {
