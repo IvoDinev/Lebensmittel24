b72968d623aeb46417313053b44b1ebed757bde3
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2020-01-09 09:32:15 +0100

    added total price, added number of products selected

--- app/src/js/state.js
+++ app/src/js/state.js
@@ -4,11 +4,13 @@ let fruitsAndVegs = [
   {
     name: 'banana',
     img:
-      'https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SL1500_.jpg'
+      'https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SL1500_.jpg',
+    price: 1
   },
   {
     name: 'apples',
-    img: 'https://www.kroger.com/product/images/xlarge/front/0000000004129'
+    img: 'https://www.kroger.com/product/images/xlarge/front/0000000004129',
+    price: 1.5
   }
 ];
 
@@ -17,54 +19,148 @@ let milkproducts = [
     name: 'Weihenstephan Haltbare Milch  %3,5 Fett 1 L',
     img: './milch.png',
     price: 1.3
+  },
+  {
+    name: 'Weihenstephan Haltbare Milch %0,1 Fett 1 L',
+    img: './milch2.png',
+    price: 1.3
+  },
+  {
+    name: 'Weihenstephan Frischer Kakao 1 L',
+    img: './kakao.png',
+    price: 1.19
+  },
+  {
+    name: 'Weihenstephan Butter 250g',
+    img: './butter.png',
+    price: 3
+  },
+  {
+    name: 'Weihenstephan Rahmjoghurt 150g',
+    img: './Joghurt.png',
+    price: 0.89
+  },
+  {
+    name: 'Weihenstephan Rahm-Romadur Käse 100g',
+    img: './kaese.png',
+    price: 1.19
+  },
+  {
+    name: 'Weihenstephan Rahm-Camembert 125g',
+    img: './Kaese2.png',
+    price: 1.59
+  },
+  {
+    name: 'Weihenstephan Sahne zum Kochen 250g',
+    img: './sahne.png',
+    price: 1.17
+  },
+  {
+    name: 'Weihenstephan Buttermilch 500g',
+    img: './buttermilch.png',
+    price: 0.89
   }
 ];
+
+let totalPrice = 0;
 
 if (!state) {
   sessionStorage.setItem('state', JSON.stringify({ selectedItems: [] }));
 }
 
-let selectItems = name => {
+let selectItems = (name, price) => {
   let input = document.getElementById(`input_${name}`);
-  let value = input.value;
-  let state = sessionStorage.getItem('state');
+  if (input.value > 100) {
+    alert('Maximal 100 Produkte können auf einmal gewählt werden');
+    return;
+  }
+  if (input.value <= 0) {
+    alert('Anzahl muss eine positive Zahl sein!');
+    return;
+  }
+  if (input.value % 10 > 0) {
+    alert('Anzahl muss eine ganze Zahl sein!');
+    return;
+  }
+  value = input.value;
+  state = sessionStorage.getItem('state');
   state = JSON.parse(state);
-  state.selectedItems.push({ name, value });
+  let itemPresents = false;
+  if (state.selectedItems.length <= 0) {
+    state.selectedItems.push({ name, value, price });
+  } else {
+    for (const item of state.selectedItems) {
+      if (item.name == name) {
+        let newValue = Number.parseInt(item.value) + Number.parseInt(value);
+        item.value = newValue;
+        itemPresents = true;
+        break;
+      }
+    }
+    if (!itemPresents) {
+      state.selectedItems.push({ name, value, price });
+    }
+  }
   sessionStorage.setItem('state', JSON.stringify(state));
+  alert(`${value}` + ' item(s) added to shopping cart!');
 };
 
-let displayMilkProducts = () => {
-  let container = document.getElementById('milkProducts');
-  milkproducts.forEach(item => {
+let displayProducts = (productsPage, products) => {
+  let container = document.getElementById(productsPage);
+  let productsCategory = [];
+
+  switch (products) {
+    case 'milkproducts':
+      productsCategory = milkproducts;
+      break;
+    case 'fruitsAndVegs':
+      productsCategory = fruitsAndVegs;
+      break;
+  }
+  productsCategory.forEach(item => {
     container.innerHTML += `<div class="items">
-    <div class="mprodukt">         
-      <img src="${item.img}">           
+    <div class="mprodukt">
+      <div class="images">        
+        <img src="${item.img}" height="250" width="200">
+      </div>           
       <p>${item.name}</p>
       <label>Anzahl        
         <input id="input_${item.name}" type="number" min="1" step="1" max="100">
-      </label>
-      <button  onclick="selectItems('${item.name}')" class="w3-button w3-xlarge w3-black" data-toggle="tooltip"
+      </label>      
+      <button  onclick="selectItems('${item.name}', '${
+      item.price
+    }')" class="w3-button w3-xlarge w3-black" data-toggle="tooltip"
       title="Zum Warenkorb hinzufügen!"> <img src="./addToShoppingCart.png"> </button>
+      <br>      
+      <label style="position: absolute; left: 110px">Einzelpreis:
+      </label>
+      <p id="price_${item.name}" style="position: absolute; left: 195px">${(
+      Math.round(item.price * 100) / 100
+    ).toFixed(2)}€
+      </p>
     </div>
   </div>`;
   });
 };
 
-let displayFruits = () => {
-  let container = document.getElementById('fruitsAndVegs');
-  fruitsAndVegs.forEach(item => {
-    container.innerHTML += `<div class="col-md-4"><img src="${item.img}" style="width: 100%; height: 50px;"><p>${item.name}</p>
-    <input id="input_${item.name}" placeholder="quantity" type="number">
-    <button onclick="selectItems('${item.name}')" class="btn btn-success">Add to shopping cart</button></div>`;
-  });
-};
 let displaySelectedItems = () => {
   let container = document.getElementById('cart-container');
   let state = sessionStorage.getItem('state');
   state = JSON.parse(state);
-  state.selectedItems.forEach(item => {
-    container.innerHTML += `<p>${item.name}</p>
-    <br>
-    <p>${item.value}</p>`;
-  });
+  if (state.selectedItems.length > 0) {
+    state.selectedItems.forEach(item => {
+      container.innerHTML += `<p>${item.name}</p>
+    <p>Anzahl: ${item.value}</p>
+    <p>Gesamtpreis: ${(Math.round(item.price * item.value * 100) / 100).toFixed(
+      2
+    )}</p>`;
+      totalPrice += item.value * item.price;
+      console.log(totalPrice);
+    });
+    container.innerHTML += `<p>Warenkorb Gesamtpreis: ${(
+      Math.round(totalPrice * 100) / 100
+    ).toFixed(2)}</p>`;
+  } else {
+    alert('No selected items');
+  }
 };
