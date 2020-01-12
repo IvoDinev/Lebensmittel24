0ef84f0b78dd3f9651ca8079f1db71077eafd929
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2020-01-12 23:18:35 +0100

    Created layout for shoppingCart page

--- app/src/js/state.js
+++ app/src/js/state.js
@@ -17,47 +17,47 @@ let fruitsAndVegs = [
 let milkproducts = [
   {
     name: 'Weihenstephan Haltbare Milch  %3,5 Fett 1 L',
-    img: './milch.png',
+    img: 'milch.png',
     price: 1.3
   },
   {
     name: 'Weihenstephan Haltbare Milch %0,1 Fett 1 L',
-    img: './milch2.png',
+    img: 'milch2.png',
     price: 1.3
   },
   {
     name: 'Weihenstephan Frischer Kakao 1 L',
-    img: './kakao.png',
+    img: 'kakao.png',
     price: 1.19
   },
   {
     name: 'Weihenstephan Butter 250g',
-    img: './butter.png',
+    img: 'butter.png',
     price: 3
   },
   {
     name: 'Weihenstephan Rahmjoghurt 150g',
-    img: './Joghurt.png',
+    img: 'Joghurt.png',
     price: 0.89
   },
   {
     name: 'Weihenstephan Rahm-Romadur Käse 100g',
-    img: './kaese.png',
+    img: 'kaese.png',
     price: 1.19
   },
   {
     name: 'Weihenstephan Rahm-Camembert 125g',
-    img: './Kaese2.png',
+    img: 'Kaese2.png',
     price: 1.59
   },
   {
     name: 'Weihenstephan Sahne zum Kochen 250g',
-    img: './sahne.png',
+    img: 'sahne.png',
     price: 1.17
   },
   {
     name: 'Weihenstephan Buttermilch 500g',
-    img: './buttermilch.png',
+    img: 'buttermilch.png',
     price: 0.89
   }
 ];
@@ -131,12 +131,14 @@ let nonFoodProducts = [
 ];
 
 let totalPrice = 0;
+let totalItemsCount = 0;
 
 if (!state) {
   sessionStorage.setItem('state', JSON.stringify({ selectedItems: [] }));
 }
 
-let selectItems = (name, price) => {
+let selectItems = (name, price, img) => {
+  console.log(img);
   let input = document.getElementById(`input_${name}`);
   value = Number.parseFloat(input.value);
   if (input.value > 100) {
@@ -155,7 +157,7 @@ let selectItems = (name, price) => {
   state = JSON.parse(state);
   let itemPresents = false;
   if (state.selectedItems.length <= 0) {
-    state.selectedItems.push({ name, value, price });
+    state.selectedItems.push({ name, value, price, img });
   } else {
     for (const item of state.selectedItems) {
       if (item.name == name) {
@@ -179,11 +181,13 @@ let removeItems = (name, selectedItemsNumber) => {
   itemsToRemove = Number.parseFloat(input.value);
   remainingItems = selectedItemsNumber - itemsToRemove;
   if (itemsToRemove > selectedItemsNumber) {
-    alert('Number of products to remove needs to be <= selected products');
+    alert(
+      'Anzahl von Produkte zum Entfernen muss kleiner oder gleich der Anzahl der gewählte Produkte sein!'
+    );
     return;
   }
   if (itemsToRemove === 0) {
-    alert('No items removed!');
+    alert('Keine Produkte entfernt!');
     return;
   }
   state = sessionStorage.getItem('state');
@@ -199,7 +203,7 @@ let removeItems = (name, selectedItemsNumber) => {
     }
   }
 
-  alert('item removed');
+  alert('Produkt(e) entfernt!');
   sessionStorage.setItem('state', JSON.stringify(state));
   location.reload();
 };
@@ -244,8 +248,8 @@ let displayProducts = (productsPage, products) => {
       <label>Anzahl        
         <input id="input_${item.name}" type="number" min="1" step="1" max="100">
       </label>      
-      <button  onclick="selectItems('${item.name}', '${
-      item.price
+      <button  onclick="selectItems('${item.name}', '${item.price}', '${
+      item.img
     }')" class="w3-button w3-xlarge w3-black" data-toggle="tooltip"
       title="Zum Warenkorb hinzufügen!"> <img src="../../common-pictures/addToShoppingCart.png"> </button>
       <br>      
@@ -261,29 +265,57 @@ let displayProducts = (productsPage, products) => {
 };
 
 let displaySelectedItems = () => {
-  let container = document.getElementById('cart-container');
+  let cartContainer = document.getElementById('cart-container');
+  cartContainer.innerHTML += `<div class="row"><h3 id="heading"></h3></div>`;
   let state = sessionStorage.getItem('state');
   state = JSON.parse(state);
+  totalItemsCount = state.selectedItems.length;
   if (state.selectedItems.length > 0) {
+    document.getElementById('heading').textContent =
+      'Ihr Warenkorb enthält ' + totalItemsCount + ' Artikel';
+    cartContainer.innerHTML += `<table class="table"><thead>
+  <tr>
+    <th scope="col"></th>
+    <th scope="col">Artikel</th>
+    <th scope="col">Menge</th>
+    <th scope="col">Stückpreis</th>
+    <th scope="col">Preis</th>
+    <th scope="col">Produkt(e) entfernen</th>
+  </tr>
+  </thead>
+  <tbody id="tbody">`;
+
     state.selectedItems.forEach(item => {
-      container.innerHTML += `<p>${item.name}</p>
-    <p id="count_${item.name}">Anzahl: ${item.value}</p>
-    <p>Gesamtpreis: ${(Math.round(item.price * item.value * 100) / 100).toFixed(
-      2
-    )}</p><label>Produkt entfernen</label>
-    <input id="input_${
-      item.name
-    }" type="number" min="1" step="1" max="100"><button onclick="removeItems('${
+      console.log(item);
+      document.getElementById('tbody').innerHTML += `<tr><td><img src="${
+        item.img
+      }" height="150" width="150"></td>
+      <td>${item.name}</td>
+      <td>${item.value}</td>
+      <td>${item.price}</td>
+      <td>${(Math.round(item.price * item.value * 100) / 100).toFixed(2)}</td>
+      <td><input id="input_${
+        item.name
+      }" type="number" min="1" step="1" max="100"><button onclick="removeItems('${
         item.name
       }', '${
         item.value
-      }')" style="size: 10px"><img style="width: 10px; heigth: 10px;" src="../common-pictures/removeProducts.jpg"></button>`;
+      }')" style="size: 10px"><img style="width: 10px; heigth: 10px;" src="../common-pictures/removeProducts.jpg"></button></td>
+      </tr>`;
       totalPrice += item.value * item.price;
     });
-    container.innerHTML += `<p>Warenkorb Gesamtpreis: ${(
+
+    cartContainer.innerHTML += `<p>Warenkorb Gesamtpreis: ${(
       Math.round(totalPrice * 100) / 100
-    ).toFixed(2)}</p>`;
+    ).toFixed(
+      2
+    )}</p><button type="button" class="btn btn-danger" onclick="emptyShoppingCart()">Warenkorb leeren</button>`;
   } else {
-    alert('No selected items');
+    alert('Keine Artikel sind gewählt!');
+    cartContainer.innerHTML += `<p>Es befinden sich keine Artikel im Warenkorb</p><button type="button" class="btn btn-danger" onclick="toHomepage()">Weiter einkaufen</button>`;
   }
+};
+
+let toHomepage = () => {
+  window.location.href = '../homepage/homepage.html';
 };
